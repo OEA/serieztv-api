643cce53b7dacf4cb3bc6f74ae342367361f8df7
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-10-23 01:10:24 +0300

    Add Star Model

---
+++ src/Models/Star.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+var Schema = mongoose.Schema;
+
+const starSchema = new Schema ({
+    name: {type: String, required: true},
+    image: {type: String, required: true},
+    birthday: {type: Date, required: true},
+    createdAt: {type: Date},
+    updatedAt: {type: Date},
+    active: {type: Boolean, required: true},
+    apiID: {type: String, required: true, select: false}
+});
+
+export default mongoose.model('Star', starSchema);
