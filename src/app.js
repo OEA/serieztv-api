ee89020ef79112c7202eb9dbc2b3811e68bb3483
Ömer Aslan <omer.aslan@ozu.edu.tr>
2016-10-22 20:02:43 +0300

    Change app.js language as es6

--- src/app.js
+++ src/app.js
@@ -1,14 +1,7 @@
+import http from 'http';
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello world\n');
+}).listen(3000);
 
-
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var port = process.env.PORT || 3000;
-var app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.listen(port, function() {
-    console.log("Listening on port 3000");
-});
+console.log('Server running at 3000 port');
