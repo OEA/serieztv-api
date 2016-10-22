5ccbee5cf72e4ae9fb18861b1cc5bce5ebe47f0f
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-10-23 01:10:14 +0300

    Add User Model

---
+++ src/Models/User.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+var Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    name: {type: String, required: true},
+    surname: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    activated: {type: Boolean, required: true},
+    activation: {type: Boolean, select: false},
+    forgotten: {type: Boolean, select: false},
+    apiID: {type: String, required: true, select: false},
+    createdAt: {type: Date},
+    updatedAt: {type: Date}
+});
+
+export default mongoose.model('User', userSchema);
