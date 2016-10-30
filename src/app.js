38477da9318a0e0f343ffe4399d8a0b3a657cb9f
Ömer Aslan <omer.aslan@ozu.edu.tr>
2016-10-30 19:38:39 +0300

    Remove dummy connection on app.js

--- src/app.js
+++ src/app.js
@@ -4,14 +4,4 @@ http.createServer((req, res) => {
     res.end('Hello world\n');
 }).listen(3000);
 
-
-mongoose.connect('mongodb://localhost:27017/seriez', (err, db) => {
-    if (err) {
-        console.log("Couldn't connect to the databese");
-    } else {
-        console.log("Connected to the database");
-
-    }
-});
-
 console.log('Server running at 3000 port');
