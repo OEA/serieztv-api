5c52b1c7c293eb3908b5f51122986010e08572d7
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-11-01 19:20:19 +0200

    Delete surname

--- src/Models/User.js
+++ src/Models/User.js
@@ -3,7 +3,6 @@ var Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
     name: {type: String, required: true},
-    surname: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     activated: {type: Boolean, required: true},
