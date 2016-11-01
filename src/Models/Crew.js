8c00274b094923c33d11a041813481cd711a0d59
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-11-01 19:18:38 +0200

    Update birthday as not required

--- src/Models/Crew.js
+++ src/Models/Crew.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 const crewSchema = new Schema ({
     name: {type: String, required: true},
     image: {type: String, required: true},
-    birthday: {type: Date, required: true},
+    birthday: {type: Date},
     job: {type: String, required: true},
     department: {type: String, required: true},
     apiID: {type: String, required: true, select: false},
--- src/Models/Star.js
+++ src/Models/Star.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 const starSchema = new Schema ({
     name: {type: String, required: true},
     image: {type: String, required: true},
-    birthday: {type: Date, required: true},
+    birthday: {type: Date},
     createdAt: {type: Date},
     updatedAt: {type: Date},
     active: {type: Boolean, required: true},
