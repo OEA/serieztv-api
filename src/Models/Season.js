c6494d3cfc9ce061013656648cd5aee62e0a5916
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-11-01 19:20:53 +0200

    Update seriesID as reference

--- src/Models/Season.js
+++ src/Models/Season.js
@@ -6,13 +6,12 @@ const seasonSchema = new Schema ({
     name: {type: String, required: true},
     number: {type: Number, required: true},
     poster: {type: String, required: true},
-    seriesID: {type: Number, required: true},
+    seriesID: {type: mongoose.Schema.ObjectId, ref: 'Series', required: true},
     overview: {type: String, required: true},
     airDate: {type: Date, required: true},
     apiID: {type: String, required: true, select: false},
     createdAt: {type: Date},
-    updatedAt: {type: Date},
-    active: {type: Number, required: true}
+    updatedAt: {type: Date}
 });
 
 export default mongoose.model('Season', seasonSchema);
