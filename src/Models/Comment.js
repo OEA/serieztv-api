59634f05f0d2a51c1f57f6fe790d9726b353ebd0
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-11-02 20:37:55 +0200

    Update comment model

--- src/Models/Comment.js
+++ src/Models/Comment.js
@@ -7,9 +7,9 @@ import mongoose from 'mongoose'
 var Schema = mongoose.Schema;
 
 const commentScheme =  new Schema ({
-    user: {type: mongoose.Schema.ObjectId, ref: 'User'},
+    user: {type: Schema.Types.ObjectId, ref: 'User'},
     comment: {type: String, required: true},
-    object: {type: mongoose.Schema.ObjectId, $or: [{ref: 'Movie'}, {ref: 'Series'}]},
+    object: {type: Schema.Types.ObjectId, $or: [{ref: 'Movie'}, {ref: 'Series'}]},
     activate: {type: Boolean, required: true, default: true},
     apiID: {type: String, required: true, select: false},
     createdAt: {type: Date},
