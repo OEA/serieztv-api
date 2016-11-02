d74cc3aeb747cfbf141428f6640d582bb4f3d7d9
Ömer Aslan <omer.aslan@ozu.edu.tr>
2016-11-02 21:21:33 +0300

    Change seriesID to series property

--- src/Models/Season.js
+++ src/Models/Season.js
@@ -6,12 +6,15 @@ const seasonSchema = new Schema ({
     name: {type: String, required: true},
     number: {type: Number, required: true},
     poster: {type: String, required: true},
-    seriesID: {type: mongoose.Schema.ObjectId, ref: 'Series', required: true},
+    series: {type: mongoose.Schema.ObjectId, ref: 'Series', required: true},
     overview: {type: String, required: true},
     airDate: {type: Date, required: true},
     apiID: {type: String, required: true, select: false},
     createdAt: {type: Date},
     updatedAt: {type: Date}
 });
+
+
+seasonSchema.set('toJSON', { getters: true });
 
 export default mongoose.model('Season', seasonSchema);
