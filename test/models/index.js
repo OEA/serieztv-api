e49b79ba6a5a42bc6341a97495352fa56c718fc1
Ömer Aslan <omer.aslan@ozu.edu.tr>
2016-11-02 21:32:32 +0300

    Fix typos on test index

--- test/models/index.js
+++ test/models/index.js
@@ -4,8 +4,8 @@
 
 import './user.test';
 import './crew.test';
-import './character.test'
-import './genre.test'
-import './star.test'
-import './comment.test'
+import './character.test';
+import './genre.test';
+import './star.test';
+import './comment.test';
 import './series.test';
