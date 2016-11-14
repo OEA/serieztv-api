3132ed340f11cf4701ada7cd16b5372c862c7743
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-11-14 12:19:37 +0200

    Improve login service tests

--- test/services/login.test.js
+++ test/services/login.test.js
@@ -3,52 +3,115 @@
  */
 import assert from 'assert';
 import mongoose from 'mongoose';
+import User from '../../src/Models/User';
 import Login from '../../src/Services/User/Login';
 import bluebird from 'bluebird';
 
+bluebird.promisifyAll(mongoose);
+
 describe('Login', ()=> {
 
-
+    before((done) => {
+        User.remove({}).then(()=> {
+            done();
+        });
+    });
 
     it('should register new user to the system', (done) => {
-        var result = Login.register("John Doe", "jd", "jdoe@gmail.com", "jdrocks", false, "1");
-        assert.equal(result.name, "John Doe");
-        done();
+        Login.register("John Doe", "jd", "jdoe@gmail.com", "jdrocks", false, "1")
+            .then( (result) => {
+            console.log(result);
+            assert.equal(result.name, "John Doe");
+            done();
+        });
+
     });
 
     it('should fail the registration for not unique email', (done) => {
-        var result = Login.register("Jane Doe", "jad", "jdoe@gmail.com", "jdrocks", false, "1");
-        assert.equal(result, "Email is not unique");
-        done();
+        Login.register("Jane Doe", "jad", "jdoe@gmail.com", "jdrocks", false, "1")
+            .then( (result) => {
+
+                console.log(result);
+             })
+
+            .catch(Login.CannotCreateUser, (error) => {
+                console.log("err " + error);
+                assert.equal(error, "Cannot create user");
+                done();
+            })
+            .catch(Login.NotUniqueEmail, (error) => {
+
+                console.log("err " + error);
+                assert.equal(error, "Email is not unique");
+                done();
+            });
+
     });
 
     it('should fail the registration for not unique username', (done) => {
-        var result = Login.register("Jane Doe", "jd", "jadoe@gmail.com", "jdrocks", false, "1");
-        assert.equal(result, "Username is not unique");
-        done();
+        Login.register("Jane Doe", "jd", "jadoe@gmail.com", "jdrocks", false, "1")
+            .then( (result) => {
+
+
+            })
+            .catch(Login.CannotCreateUser, (error) => {
+                console.log("err " + error);
+                assert.equal(error, "Cannot create user");
+                done();
+            })
+            .catch(Login.NotUniqueUsername, (error) => {
+
+                console.log("err " + error);
+                assert.equal(error, "Username is not unique");
+                done();
+            });
     });
 
     it('should fail the registration for missing argument', (done) => {
-        var result = Login.register("John Doe", "jd", "johndoe@gmail.com", "jdrocks", "1");
-        assert.equal(result, "Could not create user");
-        done();
+        Login.register("John Doe", "jd", "johndoe@gmail.com", "jdrocks", "1")
+            .then( (result) => {
+
+
+            })
+            .catch(Login.CannotCreateUser, (error) => {
+                console.log("err " + error);
+                assert.equal(error, "Cannot create user");
+                done();
+            })
     });
 
     it('should login to the system', (done) => {
-        var result = Login.login("johndoe@gmail.com", "jdrocks");
-        assert.equal(result.name, "John Doe");
-        done();
+        Login.login("jdoe@gmail.com", "jdrocks")
+            .then( (result) => {
+                console.log(result);
+                assert.equal(result.name, "John Doe");
+                done();
+            });
     });
 
     it('should fail login to the system for wrong password', (done) => {
-        var result = Login.login("johndoe@gmail.com", "jdrock");
-        assert.equal(result, "Wrong password");
-        done();
+        Login.login("jdoe@gmail.com", "jdrock")
+            .then( (result) => {
+
+
+            })
+            .catch( (error) => {
+                console.log("err " + error);
+                assert.equal(error, "Wrong Password");
+                done();
+            });
     });
 
     it('should fail login to the system for not found email', (done) => {
-        var result = Login.login("johndo@gmail.com", "jdrocks");
-        assert.equal(result, "User does not exist");
-        done();
+        Login.login("jadoe@gmail.com", "jdrock")
+            .then( (result) => {
+
+
+            })
+            .catch( (error) => {
+                console.log("err " + error);
+                assert.equal(error, "User does not exist");
+                done();
+            });
     });
 });
