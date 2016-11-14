2caed735bd798d000510f4ec4c265717e8b307b0
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-11-14 12:19:53 +0200

    Add login service

--- src/Services/User/Login.js
+++ src/Services/User/Login.js
@@ -3,29 +3,120 @@
  */
 import mongoose from 'mongoose';
 import User from '../../Models/User';
+import Promise from 'bluebird';
+
+Promise.promisifyAll(mongoose);
+
+Promise.config({
+    cancellation: true
+});
+
+const Messages = {
+    NOT_UNIQUE_EMAIL: "Email is not unique",
+    NOT_UNIQUE_USERNAME: "Username is not unique",
+    WRONG_PASSWORD: "Wrong Password",
+    CANNOT_CREATE_USER: "Could not create user",
+    USER_NOT_EXIST: "User does not exist",
+};
+
 
 class Login {
 
     static register(name, username, email, password, activated, apiID) {
-        const user = new User();
-        return user;
+
+        return new Promise((resolve, reject) => {
+            if (!this.checkUniqueEmail(email)) {
+                reject(this.NotUniqueEmail());
+
+            }  else if (!this.checkUniqueUsername(username)) {
+                reject(this.NotUniqueUsername());
+
+            } else {
+                const user = new User({
+                    name: name,
+                    username: username,
+                    email: email,
+                    password: password,
+                    activated: activated,
+                    apiID: apiID
+                });
+
+                user.save( (error) => {
+                    if (error) {
+                        reject(this.CannotCreateUser());
+
+                    } else {
+                        resolve(user);
+                    }
+                });
+            }
+        });
     }
 
+    static WrongPasswordError() {
+        this.name = "Wrong Password";
+    }
+
+
+
+    static NotUniqueEmail() {
+    return "Email is not unique";
+    }
+
+
+    static NotUniqueUsername() {
+    return "Username is not unique";
+}
+
+
+    static CannotCreateUser() {
+    return "Cannot create user";
+}
+
+
+
+
     static checkUniqueEmail(email) {
-        return false;
+        User.find({email:email}).limit(1).exec((error, users)=> {
+            return false;
+        });
+        return true;
     }
 
     static checkUniqueUsername(username) {
-        return false;
+
+        User.find({username:username}).limit(1).exec((error, users)=> {
+            return false;
+        });
+        return true;
     }
 
-    static checkPassword(password) {
+    static checkPassword(email, password) {
+        User.find({email:email}).limit(1).exec((error, users)=> {
+            if (users[0].password == password) {
+                return true;
+            }
+        });
         return false;
     }
 
     static login(email, password) {
-        const user = new User();
-        return user;
+
+        return new Promise((resolve, reject) => {
+            User.find({email:email}).limit(1).exec((error, users)=> {
+                if (error || users.length < 1) {
+                    reject(Messages.USER_NOT_EXIST);
+                } else {
+                    if (this.checkPassword(users[0].email, password)) {
+                        resolve(users[0]);
+                    } else {
+                        reject(Messages.WRONG_PASSWORD);
+                    }
+                }
+            });
+
+        });
+
     }
 
 
