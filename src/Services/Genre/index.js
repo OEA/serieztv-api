5f7cfead927a3c6d09795fa05d5c6f13f54b2186
goktugberkulu <goktug.ulu@ozu.edu.tr>
2016-11-16 12:04:21 +0200

    Add genre service

--- src/Services/Genre/index.js
+++ src/Services/Genre/index.js
@@ -8,15 +8,70 @@ import Promise from 'bluebird';
 
 Promise.promisifyAll(mongoose);
 
+const GenreErrorMessages = {
+    GENRE_ALREADY_EXISTS: "Genre already exists",
+    CANNOT_CREATE_GENRE: "Could not create genre",
+    GENRE_NOT_EXIST: "Genre does not exist"
+};
+
 class GenreService {
 
-
-    static createGenre(Genre genre) {
-
+    static create(genre) {
+        return new Promise((resolve, reject) => {
+            Genre.find({name: genre.name}).count((error, count)=> {
+                if (count > 0 || error) {
+                    reject(GenreErrorMessages.GENRE_ALREADY_EXISTS);
+                }
+            });
+            genre.save((error) => {
+                if (error) {
+                    reject(GenreErrorMessages.CANNOT_CREATE_GENRE);
+                } else {
+                    resolve(genre);
+                }
+            });
+        });
     }
 
+    static search(genreName) {
+        return new Promise((resolve, reject) => {
+            Genre.find({name:genreName}).count((error, count) => {
+                if (count == 0 || error) {
+                    reject(GenreErrorMessages.GENRE_NOT_EXIST);
+                }
+            }).then((count) => {
+                Genre.find({name: genreName}, (error, genres) => {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        resolve(genres[0]);
+                    }
+                });
+            });
 
+        });
+    }
 
+    static delete(genreName) {
+        return new Promise((resolve, reject) => {
+           Genre.find({name:genreName}).count((error, count) => {
+               if (count == 0 || error) {
+                   reject(GenreErrorMessages.GENRE_NOT_EXIST);
+               }
+           }).then((test) => {
+               Genre.findOneAndRemove({name: genreName}, (error, genre) => {
+                   console.log("test");
+                   if (error) {
+                       console.log(error);
+                   } else {
+                       resolve(genre);
+                   }
+               });
+           });
+
+        });
+
+    }
 }
 
 export default GenreService;
