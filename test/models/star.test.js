db604a3285a8b0c64418fecd19e14d3997c72e5d
Ömer Aslan <omer.aslan@ozu.edu.tr>
2016-11-17 09:20:10 +0300

    Update star tests

--- test/models/star.test.js
+++ test/models/star.test.js
@@ -2,12 +2,41 @@
  * Created by gbu on 01/11/2016.
  */
 
-import assert from 'assert';
+import { expect } from 'chai';
 import mongoose from 'mongoose';
 import Star from '../../src/Models/Star';
 import bluebird from 'bluebird';
 
 mongoose.Promise = bluebird;
+
+const KitHarrington = new Star({
+    name: 'Kit Harrington',
+    image: 'image',
+    active: true,
+    apiID: '1'
+});
+
+const EmiliaClark = new Star({
+    name: 'Emilia Clark',
+    image: 'image',
+    active: true,
+    apiID: '2'
+});
+
+const SeanBean = new Star({
+    name: 'Sean Bean',
+    image: 'image',
+    active: true,
+    apiID: '3'
+});
+
+const MarkHamill = new Star({
+    name: 'Mark Hamill',
+    image: 'image',
+    active: true,
+    apiID: '4'
+});
+
 describe('Star', ()=> {
     before((done) => {
         Star.remove({}).then(()=> {
@@ -15,24 +44,21 @@ describe('Star', ()=> {
         });
     });
 
-    it('should create and save new star', (done) => {
-        const star = new Star({
-            name: 'Mark Hamill',
-            image: 'image',
-            active: true,
-            apiID: '1'
+    it('should create and save new stars', (done) => {
+        Promise.all([
+            KitHarrington.save(),
+            EmiliaClark.save(),
+            SeanBean.save(),
+            MarkHamill.save()
+        ]).then(()=>{
+            done();
         });
-        star.save((error) => {
-            if (error) done(error);
-            else done();
-        });
-
     });
 
     it('should return created star that is Mark Hamill', (done) => {
-        Star.find({name: 'Mark Hamill'}).limit(1).exec((error, stars)=> {
-            const star = stars[0];
-            assert.equal(star.name, 'Mark Hamill');
+        Star.findOne({name: 'Mark Hamill'}).limit(1).exec((error, markHamill)=> {
+            expect(markHamill).not.to.be.null;
+            expect(markHamill.name).to.be.equal('Mark Hamill');
             done();
         });
     });
