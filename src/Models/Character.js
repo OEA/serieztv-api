764c09371e5fa8601f620a899fa0d1e309fcfaeb
Ömer Aslan <omer.aslan@ozu.edu.tr>
2016-12-18 19:39:54 +0300

    Change image as not required

--- src/Models/Character.js
+++ src/Models/Character.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 const characterSchema =  new Schema ({
     star: {type: Schema.Types.ObjectId, ref: 'Star', required: true},
     characterName: {type: String, required: true},
-    characterImage: {type: String, required: true},
+    characterImage: {type: String},
     apiID: {type: String, required: true, select: false},
     createdAt: {type: Date},
     updatedAt: {type: Date}
