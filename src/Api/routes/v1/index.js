4a00b54117560109379075c1e5c94461b15fdf18
Ömer Aslan <omer.aslan@ozu.edu.tr>
2016-12-28 11:23:42 +0300

    Improve routes

--- src/Api/routes/v1/index.js
+++ src/Api/routes/v1/index.js
@@ -1,55 +1,22 @@
 /**
  * Created by omer on 06/12/2016.
  */
-import Login from '../../../Services/User/Login';
-import User from '../../../Models/User';
+import request from 'request';
+import TMDB from '../../../Crawlers/TMDB';
 
 module.exports = function (app) {
     app.get('/v1/hello-world', (req, res) => {
-        res.json({
-            hello: 'world'
+
+    });
+
+    app.get('/v1/test', (req, res) => {
+        let tmdb = new TMDB();
+        tmdb.getFilmName("205596", (movie) => {
+            res.json(movie);
+        }, (error) => {
+            res.json(error);
         });
+
     });
 
-    app.get('/v1/usertest', (req, res) => {
-        Login.login("deathstar@gmail.com", "1234")
-            .then( (result) => {
-                console.log(result);
-                res.json({
-                    hello: 'world'
-                });
-
-            })
-            .catch( (error) => {
-                console.log(error);
-                res.json({
-                    hello: 'world'
-                });
-            });
-    });
-
-    app.get('/v1/adduser', (req, res) => {
-
-        const user = new User({
-            name: 'Jon Snow',
-            username: 'stark',
-            email: 'deathstar@gmail.com',
-            password: '1234',
-            activated: true,
-            apiID: '1'
-        });
-        Login.register(user)
-            .then((user) => {
-                console.log(user);
-                res.json({
-                    hello: 'world'
-                });
-            })
-            .catch((error) => {
-            console.log(error);
-                res.json({
-                    hello: 'world'
-                });
-            });
-    });
 };
