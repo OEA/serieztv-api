68dc3e6e65faddc4a3a5507109bdca5593886c00
Ömer Aslan <omer.aslan@ozu.edu.tr>
2017-01-02 11:47:22 +0300

    Integrate new routes

--- src/Api/routes/v1/movie/index.js
+++ src/Api/routes/v1/movie/index.js
@@ -3,12 +3,82 @@
  */
 
 import MovieService from '../../../../Services/Movie';
+import Promise from 'bluebird';
 
 module.exports = function (app) {
     app.get('/v1/movie/list', (req, resp) => {
         MovieService.getMovies()
             .then((movies) => {
                 resp.json(movies);
-            })
+            });
     });
+
+
+
+    //It can be limited by limit query
+    app.get('/v1/movie/top', (req, resp) => {
+        let limit = req.query.limit;
+        if (limit) {
+            MovieService.getTopImdbMovies(Number(limit))
+                .then((movies) => {
+                    resp.json(movies);
+                });
+        } else {
+            MovieService.getTopImdbMovies()
+                .then((movies) => {
+                    resp.json(movies);
+                });
+        }
+
+    });
+
+    //It can be limited by limit query
+    app.get('/v1/movie/recent', (req, resp) => {
+        let limit = req.query.limit;
+        if (limit) {
+            MovieService.getRecentMovies(Number(limit))
+                .then((movies) => {
+                    resp.json(movies);
+                });
+        } else {
+            MovieService.getRecentMovies()
+                .then((movies) => {
+                    resp.json(movies);
+                });
+        }
+
+    });
+
+
+    app.get('/v1/movie/find/:id', (req, resp) => {
+        let id = req.params.id;
+
+        MovieService.getMovieFromId(id)
+            .then((movie) => {
+                resp.json(movie);
+            });
+
+    });
+
+
+    app.get('/v1/movie/search', (req, resp) => {
+        let genre = req.query.genre;
+        let name = req.query.name;
+        if (genre) {
+            MovieService.getMovieFromGenre(genre)
+                .then((movies) => {
+                    resp.json(movies);
+                })
+                .catch((error) => {
+                    resp.json({error: error});
+                })
+        } else if (name) {
+
+        } else {
+            resp.json({error: "Specify the search."});
+        }
+
+
+    });
+
 };
